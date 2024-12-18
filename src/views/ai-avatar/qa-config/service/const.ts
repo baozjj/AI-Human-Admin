6f58bdeccc43b0e0491d909464561111bd8b2396
baozj <1670370148@qq.com>
2024-12-19 00:25:30 +0800

    feat：问答管理

---
+++ src/views/ai-avatar/qa-config/service/const.ts
@@ -0,0 +1,67 @@
+export const mockData = [
+  {
+    index: 'p001',
+    id: 1,
+    userName: '物业报修查询',
+    updateBy: 'liming',
+    nickName:
+      '根据用户提出的 “&{content}”，以及知识库内容“&{repairInfo}”生成包含维修内容、时间和地点的 JSON 格式响应。',
+    userPhone: '2024-01-15 10:35:45',
+    userEmail: '2024-01-16 16:20:12',
+    status: 1
+  },
+  {
+    index: 'p002',
+    id: 1,
+    userName: '社区活动安排',
+    updateBy: 'wangyi',
+    nickName: '针对用户查询 “&{content}”，以及知识库内容“&{repairInfo}”生成活动名称、时间和参与方式的 JSON 格式结果。',
+    userPhone: '2024-01-15 10:33:02',
+    userEmail: '2024-01-17 12:35:07',
+    status: 0
+  },
+  {
+    index: 'p003',
+    id: 1,
+    userName: '物业费缴纳通知',
+    updateBy: 'chenzhengyu',
+    nickName:
+      '根据用户提出的“&{content}”，以及知识库内容“&{repairInfo}”生成缴费金额、缴费截止时间和支付方式的 JSON 格式响应。',
+    userPhone: '2024-01-18 11:24:34',
+    userEmail: '2024-01-20 10:35:74',
+    status: 1
+  },
+  {
+    index: 'p004',
+    id: 1,
+    userName: '公共设施开放时间查询',
+    updateBy: 'liqinwen',
+    nickName:
+      '根据用户提出的 “&{content}”，以及知识库内容“&{repairInfo}”生成设施开放时间、使用规则和当前状态，并生成 JSON 格式响应。。',
+    userPhone: '2024-01-24 16:24:02',
+    userEmail: '2024-01-27 11:22:32',
+    status: 1
+  },
+  {
+    index: 'p005',
+    id: 1,
+    userName: '物业服务',
+    updateBy: 'taojie',
+    nickName:
+      '你现在是五星田里小区物业客服，根据用户提出的 “&{content}”，以及知识库内容“&{repairInfo}”生成 JSON 格式响应。',
+    userPhone: '2024-01-25 14:42:34',
+    userEmail: '2024-02-12 15:34:15',
+    status: 1
+  },
+  {
+    index: 'p006',
+    id: 1,
+    userName: '物业管理公告',
+    updateBy: 'chenshuo',
+    nickName:
+      '用户咨询 “&{content}”，以及知识库内容“&{repairInfo}”生成公告标题、发布时间和公告内容摘要的 JSON 输出格式。',
+    userPhone: '2024-02-13 13:24:24',
+    userEmail: '2024-02-15 16:24:12',
+    status: 1
+  }
+];
